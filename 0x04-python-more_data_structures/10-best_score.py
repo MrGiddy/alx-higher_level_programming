f96cba44f104ba96198c27875e5094dc9f731836
Matumbai Binale <binalematumbai@gmail.com>
2024-01-10 10:15:44 -0800

    (10-best_score.py): Refactor to more logical code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@
 def best_score(a_dictionary: dict) -> str:
     if (a_dictionary is None):
         return None
-
-    best_score_key = max(a_dictionary, key=a_dictionary.get)
-    return best_score_key
+    best_score = max(a_dictionary.values(), default=None)
+    for key, value in a_dictionary.items():
+        if (value == best_score):
+            return key
