a9ffe4b895ff4aacc769d5f1e4a6c23903e5f72d
Matumbai Binale <binalematumbai@gmail.com>
2024-01-10 07:35:34 -0800

    (8-simple_delete.py): Delete key in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary: dict, key: str, value: any) -> None:
+def update_dictionary(a_dictionary: dict, key: str, value: any) -> dict:
     a_dictionary[key] = value
     return a_dictionary
