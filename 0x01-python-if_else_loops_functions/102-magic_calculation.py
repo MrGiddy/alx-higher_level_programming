782eb137c0e35f8023295cbb75e9331671498162
Matumbai Binale <binalematumbai@gmail.com>
2024-01-14 01:02:53 +0000

    (102-magic_calculation.py): Write a python function to match provided bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Build python code from given bytecode"""
+    if a < b:
+        return c
+    if c > b:
+        return a + b
+    return a * b - c
