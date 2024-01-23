045f0d2c855e17d180365dd4a18210aa2543d2db
Matumbai Binale <binalematumbai@gmail.com>
2024-01-23 15:26:26 -0800

    (1-square.py): Instantiate square with private instance - size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Empty class Square."""
+
+    def __init__(self, size):
+        """Initialize the size of anew square
+
+        Args:
+            size (int): Size of a new square
+        """
+        self.__size = size
