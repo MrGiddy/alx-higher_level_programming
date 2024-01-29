485c026c243656943faab30a4077b1fda355af7b
Matumbai Binale <binalematumbai@gmail.com>
2024-01-29 15:06:18 -0800

    (5-text_indentation.py): Print a text with 2 new lines after delims '.', '?', and ':'

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+""" Has text_indentation function """
+
+
+def text_indentation(text):
+    """
+        Prints a text with 2 new lines after each delimiter: '.' , '?' and ':'
+
+        Skips all spaces folowing a delimiter
+
+        Args:
+            text (str): The input text
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+    after_delimiter = False
+
+    for char in text.strip(' '):
+        if char in ['.', '?', ':']:
+            after_delimiter = True
+            result += char
+            result += '\n\n'
+        elif char == '\n':
+            result += char
+            after_delimiter = True
+        elif char == ' ' and after_delimiter:
+            continue
+        else:
+            result += char
+            after_delimiter = False
+
+    print(result, end="")
