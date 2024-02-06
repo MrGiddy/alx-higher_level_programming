5f023f8bca20debb84a9fea7d6d6624eaed51369
Matumbai Binale <binalematumbai@gmail.com>
2024-02-05 17:44:17 -0800

    (6-base_geometry.py): Raise an exception in area() attribute of BaseGeoemetry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Am empty class; BaseGeometry """
+
+
+class BaseGeometry:
+    """ BaseGeoemetry class """
+    def area(self):
+        """ Raises an exception """
+        raise Exception("area() is not implemented")
