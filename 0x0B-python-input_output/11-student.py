4b960c3ab14aef7e49be24bf609991c9e4788e63
Matumbai Binale <binalematumbai@gmail.com>
2024-02-07 05:37:38 -0800

    (11-student.py): Refactor to dynamically replace attributes of instance of Student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,11 +5,13 @@
 class Student:
     """ class Student """
     def __init__(self, first_name, last_name, age):
+        """ Iniialize a new Student """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """" Retrieve __dict__ of Student instance """
         if isinstance(attrs, list) and\
                 (isinstance(attrs[i], str) for i in attrs):
             return {key: self.__dict__[key] for key in attrs
@@ -17,4 +19,6 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dict__ = json
+        """ Replace attributes of instance Student """
+        for key, value in json.items():
+            setattr(self, key, value)
