27eb641767e099e46dc32b8a81d5c3323c1edd20
Matumbai Binale <binalematumbai@gmail.com>
2024-04-10 19:49:07 +0300

    (102-concat.js): Redo solution and make the file executable

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,16 @@
 #!/usr/bin/node
 /*
  * Concats two files
+ * First arg, argv[2]: first source file
+ * Second arg, argv[3]: second source file
+ * Third arg, argv[4]: destination file
+ *
  */
-const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+
+/* Import fs module */
+const fs = require('node:fs');
+
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf8');
+
+fs.writeFileSync(process.argv[4], file1 + file2);
