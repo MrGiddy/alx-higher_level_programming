834f3c7b27d9a4c581480cce4fce7ce211f7f00e
Matumbai Binale <binalematumbai@gmail.com>
2024-04-10 19:11:36 +0300

    (101-sorted.js): Redo the solution and make the file executable

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+/*
+ * Imports a dictionary of occurrences by user id
+ * Computes a dictionary of user ids by occurrence
+ *
+ */
+const { dict } = require('./101-data');
+const newDict = {}; // will contain ids by occurrences
+
+for (const [key, value] of Object.entries(dict)) {
+  if (!(value in newDict)) {
+    newDict[value] = [];
   }
+  newDict[value].push(key);
 }
+
 console.log(newDict);
