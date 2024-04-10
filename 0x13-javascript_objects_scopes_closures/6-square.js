40ac717a0b513216cdb902f3793eacb79d17004a
Matumbai Binale <binalematumbai@gmail.com>
2024-04-10 14:50:08 +0300

    (6-square.js): Fix to import from 5-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
+class Square extends require('./5-square') {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
     if (c === undefined) {
-      c = this.print();
-    }
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += c;
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += c;
+        }
+        console.log(row);
       }
-      console.log(row);
     }
   }
 }
