d24451a8cb69d69c7f2eddce6f4ef04f72ea0683
Matumbai Binale <binalematumbai@gmail.com>
2024-04-27 01:57:01 +0300

    (task 0): What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a function fetching a url"""
+import urllib.request
+
+
+def main(url):
+    """Fetches a url"""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        cont = response.read()
+
+    print("Body response:$")
+    print('\t- type:', type(cont))
+    print('\t- content:', cont)
+    print('\t- utf8 content:', cont.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    main('https://alx-intranet.hbtn.io/status')
