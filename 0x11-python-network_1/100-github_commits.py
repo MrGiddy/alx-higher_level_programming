892a9d06659825b7ef970346d40845e5619f83e4
Matumbai Binale <binalematumbai@gmail.com>
2024-04-27 18:04:32 +0300

    (task 10): Use lexicographical sort on date

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,7 +2,6 @@
 """Defines a script that lists 10 commits of a GitHub repository"""
 import sys
 import requests
-from datetime import datetime
 
 
 def display_gh_commits():
@@ -13,12 +12,12 @@ def display_gh_commits():
     gh_api_url = f'https://api.github.com/repos/{owner}/{repo}/commits'
 
     # Send a GET request for commits to github api
-    req = requests.get(gh_api_url)
+    resp = requests.get(gh_api_url)
 
     # Create an empty list to store data we want from commits
     commits_list = []
     # Iterate the commits list from the response
-    for commit in req.json():
+    for commit in resp.json():
         # Store the desired data of a commit in a temporary dictionary
         temp_dict = {}
         temp_dict['sha'] = commit.get('sha')
@@ -28,10 +27,8 @@ def display_gh_commits():
         commits_list.append(temp_dict)
 
     # Sort the list of commits data extracted by date ascending
-    commits_list.sort(
-        key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%SZ'),
-        reverse=True
-    )
+    # Note: Lexicographical sort of ISO 8601 strs is the same as datetime sort
+    commits_list.sort(key=lambda x: x['date'], reverse=True)
 
     # Iterate through only 10 of the sorted commits data
     for commit in commits_list[:10]:
