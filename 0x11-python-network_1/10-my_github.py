203866d6d263656af096d3df139bef67f2f7c46a
Matumbai Binale <binalematumbai@gmail.com>
2024-04-27 06:20:06 +0300

    (task 10): Include auth parameter in request

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Defines a script that uses GitHub API to display id"""
+import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
 
-def main(username, password, url):
+def main():
     """Uses GitHub API to display a User's id"""
+    username, password = sys.argv[1:]
+    gh_api = 'https://api.github.com/user'
+
     auth = HTTPBasicAuth(username, password)
-    req = requests.get(url)
+    req = requests.get(gh_api, auth=auth)
     print(req.json().get("id"))
-    
+
 
 if __name__ == '__main__':
-    import sys
-    username, password = sys.argv[1:]
-    url = 'https://api.github.com/user'
-    main(username, password, url)
+    main()
