8fb4fc45879d3edd100ab39ac35b55b5b95e81f4
Matumbai Binale <binalematumbai@gmail.com>
2024-05-23 01:09:15 +0300

    (task 2): Display a GET request's status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Displays the status code of a  GET request
+
+// Import the request module
+const request = require('request');
+
+// Get the url to query
+const url = process.argv[2];
+
+request(url, (err, resp) => {
+  // Display the error if it occurred
+  if (err) console.log(err);
+  // Display the status code of the response
+  console.log(resp.statusCode);
+});
